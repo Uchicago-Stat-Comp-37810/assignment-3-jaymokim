3d551217226258733082071bf2f25feddff2d867
jaymokim <jaymokim@uchicago.edu>
2018-10-23 19:59:31 -0500

    initial template
    just wrote my name on the template

---
+++ hw_3.py
@@ -0,0 +1,53 @@
+# Name: Jaymo Kim
+# hw3.py
+
+##### Template for Homework 3, exercises 1 -  ######
+
+
+# ********** Exercise 1 ********** 
+
+# Define is_divisible function here
+##### YOUR CODE HERE #####
+
+# Test cases for is_divisible
+## Provided for you... uncomment when you're done defining your function
+
+#print is_divisible(10, 5)  # This should return True
+#print is_divisible(18, 7)  # This should return False
+#print is_divisible(42, 0)  # What should this return?
+
+# ********** Exercise 2 ********** 
+
+# Define not_equal function here
+##### YOUR CODE HERE #####
+
+# Test cases for not_equal
+##### YOUR CODE HERE #####
+
+# ********** Exercise 3 ********** 
+
+## 1 - multadd function
+##### YOUR CODE HERE #####
+
+## 2 - Equations
+##### YOUR CODE HERE #####
+
+
+# Test Cases
+# angle_test =
+# print "sin(pi/4) + cos(pi/4)/2 is:"
+# print angle_test
+
+# ceiling_test =
+# print "ceiling(276/19) + 2 log_7(12) is:"
+# print ceiling_test
+
+
+# ********** Exercise 4 **********
+
+## 1 - rand_divis_3 function
+##### YOUR CODE HERE #####
+
+# Test Cases
+##### YOUR CODE HERE #####
+
